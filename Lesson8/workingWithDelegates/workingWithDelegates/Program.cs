d4ca4d63e06b3a4f1f5ed06443456251cb21f5b6
pizzahunter239 <pizzahunter239@gmail.com>
2025-03-03 18:12:19 -0500

    Lesson8

---
+++ Lesson8/workingWithDelegates/workingWithDelegates/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace Assignment8ex2
+{
+    public class MathSolutions
+    {
+        public delegate double SumDelegate(double x, double y);
+        public double GetSum(double x, double y)
+        {
+            return x + y;
+        }
+        public double GetProduct(double a, double b)
+        {
+            return a * b;
+        }
+        public void GetSmaller(double a, double b)
+        {
+            if (a < b)
+                Console.WriteLine($" {a} is smaller than {b}");
+            else if (b < a)
+                Console.WriteLine($" {b} is smaller than {a}");
+            else
+                Console.WriteLine($" {b} is equal to {a}");
+        }
+        static void Main(string[] args)
+        {
+            // create a class object
+            MathSolutions answer = new MathSolutions();
+            Random r = new Random();
+            double num1 = Math.Round(r.NextDouble() * 100);
+            double num2 = Math.Round(r.NextDouble() * 100);
+            Console.WriteLine("Original Method Calls");
+            Console.WriteLine($" {num1} + {num2} = {answer.GetSum(num1, num2)}");
+            Console.WriteLine($" {num1} * {num2} = {answer.GetProduct(num1, num2)}");
+            answer.GetSmaller(num1, num2);
+
+            Console.WriteLine("\nUsing Delegates");
+            SumDelegate sumDelegate = answer.GetSum;
+            double sumResult = sumDelegate(num1, num2);
+            Console.WriteLine($"{num1} + {num2} = {sumResult}");
+
+            Func<double, double, double> productFunc = answer.GetProduct;
+            double productResult = productFunc(num1, num2);
+            Console.WriteLine($"{num1} * {num2} = {productResult}");
+
+            Action<double, double> smallerAction = answer.GetSmaller;
+            smallerAction(num1, num2);
+
+            Console.WriteLine("\nUsing anonymous methods:");
+
+            Func<double, double, double> anonymousProduct = (a, b) => a * b;
+            Console.WriteLine($"{num1} * {num2} = {anonymousProduct(num1, num2)}");
+
+            Action<double, double> anonymousSmaller = (a, b) => {
+                if (a < b)
+                    Console.WriteLine($" {a} is smaller than {b}");
+                else if (b < a)
+                    Console.WriteLine($" {b} is smaller than {a}");
+                else
+                    Console.WriteLine($" {b} is equal to {a}");
+            };
+            anonymousSmaller(num1, num2);
+        }
+    }
+}
---
+++ Lesson8/workingWithEvents/workingWithEvents/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace delegatesAndEvents
+{
+    // create a delegate
+    public delegate void RaceWinnerDelegate(int winnerNumber);
+    public class Race
+    {
+        // create a delegate event object
+        public event RaceWinnerDelegate WinnerEvent;
+        public void Racing(int contestants, int laps)
+        {
+            Console.WriteLine("Ready\nSet\nGo!");
+            Random r = new Random();
+            int[] participants = new int[contestants];
+            bool done = false;
+            int champ = -1;
+            // first to finish specified number of laps wins
+            while (!done)
+            {
+                for (int i = 0; i < contestants; i++)
+                {
+
+                    if (participants[i] <= laps)
+                    {
+                        participants[i] += r.Next(1, 5);
+                    }
+                    else
+                    {
+                        champ = i;
+                        done = true;
+                        continue;
+                    }
+                }
+
+            }
+
+            TheWinner(champ);
+        }
+        private void TheWinner(int champ)
+        {
+            Console.WriteLine("We have a winner!");
+            // invoke the delegate event object and pass champ to the method
+            WinnerEvent?.Invoke(champ);
+        }
+    }
+    class Program
+    {
+        public static void Main()
+        {
+            // create a class object
+            Race round1 = new Race();
+            // register with the footRace event
+            round1.WinnerEvent += footRace;
+            // trigger the event
+            Console.WriteLine("\nFoot Race");
+            round1.Racing(5, 10);
+            // register with the carRace event
+            round1.WinnerEvent -= footRace;
+            round1.WinnerEvent += carRace;
+            //trigger the event
+            Console.WriteLine("\nCar Race");
+            round1.Racing(4, 15);
+            // register a bike race event using a lambda expression
+            round1.WinnerEvent -= carRace;
+            round1.WinnerEvent += (winner) =>
+            {
+                Console.WriteLine($"Cyclist number {winner} has won the bike race!");
+                Console.WriteLine($"Bike #{winner} finished first!");
+            };
+            // trigger the event
+            Console.WriteLine("\nBike Race");
+            round1.Racing(6, 8);
+        }
+
+        // event handlers
+        public static void carRace(int winner)
+        {
+            Console.WriteLine($"Car number {winner} is the winner.");
+        }
+        public static void footRace(int winner)
+        {
+            Console.WriteLine($"Racer number {winner} is the winner.");
+        }
+    }
+}
