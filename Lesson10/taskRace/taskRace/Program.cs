4924129a8ccf98f11a1732d8703769329cf9b24c
pizzahunter239 <pizzahunter239@gmail.com>
2025-03-31 14:51:06 -0400

    Lesson10

---
+++ Lesson10/taskRace/taskRace/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace taskRace
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ready, Set, Go...");
+            int t1Location = 0;
+            int t2Location = 0;
+            int t3Location = 0;
+            int t4Location = 0;
+            int t5Location = 0;
+
+            bool raceFinished = false;
+
+            // Creating Tasks
+            Task t1 = Task.Run(() =>
+            {
+                Thread.CurrentThread.Name = "Speedy Gonzales";
+                for (int i = 0; i < 100; i++)
+                {
+                    if (!raceFinished && t1Location < 100 && t2Location < 100 && t3Location < 100 && t4Location < 100 && t5Location < 100)
+                        MoveIt(ref t1Location, ref raceFinished);
+                }
+            });
+
+            Task t2 = Task.Run(() =>
+            {
+                Thread.CurrentThread.Name = "Road Runner";
+                for (int i = 0; i < 100; i++)
+                {
+                    if (!raceFinished && t1Location < 100 && t2Location < 100 && t3Location < 100 && t4Location < 100 && t5Location < 100)
+                        MoveIt(ref t2Location, ref raceFinished);
+                }
+            });
+
+            Task t3 = Task.Run(() =>
+            {
+                Thread.CurrentThread.Name = "Sonic the Hedgehog";
+                for (int i = 0; i < 100; i++)
+                {
+                    if (!raceFinished && t1Location < 100 && t2Location < 100 && t3Location < 100 && t4Location < 100 && t5Location < 100)
+                        MoveIt(ref t3Location, ref raceFinished);
+                }
+            });
+
+            Task t4 = Task.Run(() =>
+            {
+                Thread.CurrentThread.Name = "Flash";
+                for (int i = 0; i < 100; i++)
+                {
+                    if (!raceFinished && t1Location < 100 && t2Location < 100 && t3Location < 100 && t4Location < 100 && t5Location < 100)
+                        MoveIt(ref t4Location, ref raceFinished);
+                }
+            });
+
+            Task t5 = Task.Run(() =>
+            {
+                Thread.CurrentThread.Name = "Quicksilver";
+                for (int i = 0; i < 100; i++)
+                {
+                    if (!raceFinished && t1Location < 100 && t2Location < 100 && t3Location < 100 && t4Location < 100 && t5Location < 100)
+                        MoveIt(ref t5Location, ref raceFinished);
+                }
+            });
+
+            // Wait for any of the tasks to complete
+            Task.WaitAll(t1, t2, t3, t4, t5);
+
+            Console.WriteLine("Race has ended");
+        }
+
+        static void MoveIt(ref int location, ref bool raceFinished)
+        {
+            location++;
+            Console.WriteLine($"{Thread.CurrentThread.Name} location={location}");
+            if (location >= 100 && !raceFinished)
+            {
+                raceFinished = true;
+                Console.WriteLine($"{Thread.CurrentThread.Name} is the winner!");
+                return;
+            }
+        }
+    }
+}
---
+++ Lesson10/threadRace/threadRace/Program.cs
@@ -0,0 +1,96 @@
+﻿namespace threadRace
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Read, Set Go...");
+            int t1Location = 0;
+            int t2Location = 0;
+            int t3Location = 0;
+            int t4Location = 0;
+            int t5Location = 0;
+
+            bool raceFinished = false;
+
+            //Creating Threads
+            Thread t1 = new Thread(delegate ()
+            {
+                Thread.CurrentThread.Name = "Speedy Gonzales";
+                for (int i = 0; i < 100; i++)
+                {
+                    if (!raceFinished)
+                        MoveIt(ref t1Location, ref raceFinished);
+                }
+
+            });
+            Thread t2 = new Thread(delegate ()
+            {
+                Thread.CurrentThread.Name = "Road Runner";
+                for (int i = 0; i < 100; i++)
+                {
+                    if (!raceFinished)
+                        MoveIt(ref t2Location, ref raceFinished);
+
+                }
+            });
+            Thread t3 = new Thread(delegate ()
+            {
+                Thread.CurrentThread.Name = "Sonic the Hedgehog";
+                for (int i = 0; i < 100; i++)
+                {
+                    if (!raceFinished)
+                        MoveIt(ref t3Location, ref raceFinished);
+                }
+            });
+            Thread t4 = new Thread(delegate ()
+            {
+                Thread.CurrentThread.Name = "Flash";
+                for (int i = 0; i < 100; i++)
+                {
+                    if (!raceFinished)
+                        MoveIt(ref t4Location, ref raceFinished);
+                }
+            });
+            Thread t5 = new Thread(delegate ()
+            {
+                Thread.CurrentThread.Name = "Cade";
+                for (int i = 0; i < 100; i++)
+                {
+                    if (!raceFinished)
+                        MoveIt(ref t5Location, ref raceFinished);
+                }
+            });
+            //Executing the methods
+
+            t3.Priority = ThreadPriority.AboveNormal;
+            Console.WriteLine($"{t3.Name} has AboveNormal priority");
+
+            t1.Start();
+            t2.Start();
+            t3.Start();
+            t4.Start();
+            t5.Start();
+
+            t1.Join();
+            t2.Join();
+            t3.Join();
+            t4.Join();
+            t5.Join();
+
+            Console.WriteLine("Race has ended");
+        }
+        static void MoveIt(ref int location, ref bool raceFinished)
+        {
+            location++;
+            Console.WriteLine($"{Thread.CurrentThread.Name} location={location}");
+            if (location >= 100 && !raceFinished)
+            {
+                raceFinished = true;
+                Console.WriteLine($"{Thread.CurrentThread.Name} is the winner");
+                return;
+            }
+        }
+
+    }
+}
